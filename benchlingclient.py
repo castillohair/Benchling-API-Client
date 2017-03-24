2a704800dcd5ef098403ab42f640344828cc2a06
Sebastian M. Castillo Hair <castillohair@gmail.com>
2017-03-24 18:39:23 -0500

    Increased version number.

--- benchlingclient.py
+++ benchlingclient.py
@@ -8,7 +8,7 @@ import requests
 # Versions should comply with PEP440.  For a discussion on single-sourcing
 # the version across setup.py and the project code, see
 # https://packaging.python.org/en/latest/single_source_version.html
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 # API Key for login
 LOGIN_KEY = 'not a real key!'
