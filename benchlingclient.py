1bf856a7df9dd7cc1a2d69884eadbadde6ebf589
Sebastian M. Castillo Hair <castillohair@gmail.com>
2018-08-13 18:27:16 -0500

    Increased version number

--- benchlingclient.py
+++ benchlingclient.py
@@ -8,7 +8,7 @@ import requests
 # Versions should comply with PEP440.  For a discussion on single-sourcing
 # the version across setup.py and the project code, see
 # https://packaging.python.org/en/latest/single_source_version.html
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 # URL for API requests
 API_URL = "https://benchling.com/api/v1/"
